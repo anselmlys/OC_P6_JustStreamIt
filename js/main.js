0e028dd5773cd78f1e40cdd5db69d874e3743914
anselmlys <anselm.lisa@gmail.com>
2025-11-17 10:54:30 +0100

    Simplify Best Movie Box code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-import { displayBestMovie } from "../js/ui/best-movie-elements.js"
+import { displayBestMovieBox } from "../js/ui/best-movie-elements.js"
 import { displayMovieGrid } from "../js/ui/movie-grid-elements.js"
 import { displayDropdownOptions } from "../js/ui/genres-drop-down-menu.js" 
 
 
 function displayMovieData() {
-    displayBestMovie()
+    displayBestMovieBox()
     displayMovieGrid("best-movies", null)
     displayMovieGrid("thriller", "thriller")
     displayMovieGrid("sci-fi", "sci-fi")
--- js/ui/best-movie-elements.js
+++ js/ui/best-movie-elements.js
@@ -3,42 +3,40 @@ import { getMovieData } from "../api/full-movie-data.js"
 import { addMovieModal } from "../ui/movie-details-modal.js"
 
 
-function displayBestMovieTitle(bestMovie) {
-    let bestMovieTitle = document.createElement("h3")
-    bestMovieTitle.innerText = bestMovie.title
-    let bestMovieDetails = document.querySelector(".best-movie-details")
-    bestMovieDetails.insertBefore(bestMovieTitle, bestMovieDetails.firstChild)
+const bestMovieBoxTemplate = (() => {
+    let template = document.createElement("template")
+    template.innerHTML = `
+    <div class="best-movie-box">
+        <img class="best-movie-image">
+        <div class="best-movie-details">
+            <h3></h3>
+            <p></p>
+            <a href="#modal-movie-details" class="movie-details-button">Détails</a>
+            <div id="modal-movie-details" class="modal">
+            </div>
+        </div>
+    </div>`
+    return template
+})()
+
+function createBestMovieBox(movieData) {
+    let bestMovieBox = bestMovieBoxTemplate.content.cloneNode(true)
+
+    bestMovieBox.querySelector("h3").innerText = movieData.title
+    bestMovieBox.querySelector("p").innerText = movieData.description
+    bestMovieBox.querySelector("img").setAttribute("src", "https://picsum.photos/550/750")
+    bestMovieBox.querySelector("img").setAttribute("alt", "Affiche du film " + movieData.title)
+    bestMovieBox.querySelector("img").setAttribute("title", movieData.title)
+    addMovieModal(bestMovieBox, movieData)
+
+    return bestMovieBox
 }
 
-async function displayBestMovieSummary(bestMovie) {
-    let bestMovieSummary = document.createElement("p")
-    let movieData = await getMovieData(bestMovie)
-    bestMovieSummary.innerText = movieData.description
-    let bestMovieDetails = document.querySelector(".best-movie-details")
-    bestMovieDetails.querySelector("a").insertAdjacentElement("beforebegin", bestMovieSummary)
-}
-
-function displayBestMovieImage(bestMovie) {
-    let bestMovieImage = document.createElement("img")
-    bestMovieImage.setAttribute("src", bestMovie.image_url)
-    bestMovieImage.setAttribute("alt", "Affiche du film " + bestMovie.title)
-    bestMovieImage.setAttribute("title", bestMovie.title)
-    let bestMovieBox = document.querySelector(".best-movie-box")
-    bestMovieBox.insertBefore(bestMovieImage, bestMovieBox.firstChild)
-}
-
-async function displayBestMovieModal(section, bestMovie) {
-    let movieSection = document.getElementById(section)
-    let movieData = await getMovieData(bestMovie)
-    addMovieModal(movieSection, movieData)
-}
-
-export async function displayBestMovie() {
+export async function displayBestMovieBox() {
     let bestMovie = await getBestMovie()
+    let movieData = await getMovieData(bestMovie)
 
-    displayBestMovieTitle(bestMovie)
-    displayBestMovieSummary(bestMovie)
-    /* displayBestMovieImage(bestMovie) 
-    Do not add this function as long as error 404 still there*/
-    displayBestMovieModal("best-movie", bestMovie)
+    let bestMovieBox = createBestMovieBox(movieData)
+    let bestMovieSection = document.getElementById("best-movie")
+    bestMovieSection.append(bestMovieBox)
 }
